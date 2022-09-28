eddb9dbbbde09f6fd2ef60dec74766455b47b65d
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-09-28 15:10:14 +1000

    Add valid number function

--- demo_guessing.py
+++ demo_guessing.py
@@ -5,11 +5,22 @@ FILENAME = "secret.txt"
 
 def main():
     secret = load_number(FILENAME)
-    guess = int(input("? "))
+    guess = get_valid_number()
     while guess != secret:
         print("Guess again!")
-        guess = int(input("? "))
+        guess = get_valid_number()
     print("You got it!")
+
+
+def get_valid_number():
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            guess = int(input("? "))
+            is_valid_input = True
+        except ValueError:
+            print("Invalid integer")
+    return guess # no problem with potential undefined variable
 
 
 def load_number(filename):
