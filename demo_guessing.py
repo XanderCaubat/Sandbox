501ff717ea9340a2cc4f2032f76aad97b569c045
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-09-28 14:58:44 +1000

    Add load number function

--- demo_guessing.py
+++ demo_guessing.py
@@ -1,13 +1,22 @@
 """Guessing game demo"""
 
+FILENAME = "secret.txt"
+
 
 def main():
-    secret = 6
+    secret = load_number(FILENAME)
     guess = int(input("? "))
     while guess != secret:
         print("Guess again!")
         guess = int(input("? "))
     print("You got it!")
+
+
+def load_number(filename):
+    infile = open(filename, "r")
+    number = int(infile.read())
+    infile.close()
+    return number
 
 
 main()
